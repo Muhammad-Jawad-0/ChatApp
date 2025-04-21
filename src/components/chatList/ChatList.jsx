c0536d31169e7f08a4e5a99bb3c8c02cfb212b25
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-04-22 02:07:14 +0500

    styling chat components

--- src/components/chatList/ChatList.jsx
+++ src/components/chatList/ChatList.jsx
@@ -1,8 +1,58 @@
 import "./chatList.css"
+import SearchBar from "../../assets/search.png";
+import Plus from "../../assets/plus.png";
+import Minus from "../../assets/minus.png";
+import Avatar from "../../assets/avatar.png";
+import { useState } from "react";
 
 const ChatList = () => {
+  const [addMode,setAddMode] = useState(false)
   return (
-    <div className="chatList"> ChatList</div>
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src={SearchBar} alt="" />
+          <input type="text" placeholder="Search" />
+        </div>
+        <img onClick={() => setAddMode(!addMode)} src={addMode ? Minus : Plus} alt="" className="add" />
+      </div>
+
+      <div className="item">
+        <img src={Avatar} alt="" />
+        <div className="text">
+          <span>Muhamamd Jawad</span>
+          <p>Hi Kia haal ha?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src={Avatar} alt="" />
+        <div className="text">
+          <span>Muhamamd Jawad</span>
+          <p>Hi Kia haal ha?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src={Avatar} alt="" />
+        <div className="text">
+          <span>Muhamamd Jawad</span>
+          <p>Hi Kia haal ha?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src={Avatar} alt="" />
+        <div className="text">
+          <span>Muhamamd Jawad</span>
+          <p>Hi Kia haal ha?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src={Avatar} alt="" />
+        <div className="text">
+          <span>Muhamamd Jawad</span>
+          <p>Hi Kia haal ha?</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/userInfo/UserInfo.jsx
+++ src/components/userInfo/UserInfo.jsx
@@ -1,8 +1,22 @@
-import "./userInfo.css"
+import "./userInfo.css";
+import More from "../../assets/more.png";
+import Video from "../../assets/video.png";
+import Edit from "../../assets/edit.png";
+import Avatar from "../../assets/avatar.png";
 
- const UserInfo = () => {
+const UserInfo = () => {
   return (
-    <div className="userInfo">UserInfo</div>
-  )
-}
-export default UserInfo
+    <div className="userInfo ">
+      <div className="user">
+        <img src={Avatar} alt="" />
+        <h2>Muhammad Jawad</h2>
+      </div>
+      <div className="icons">
+        <img src={More} alt="" />
+        <img src={Video} alt="" />
+        <img src={Edit} alt="" />
+      </div>
+    </div>
+  );
+};
+export default UserInfo;
