52f8e3e54dd23d9bf10399b78bc293928f3a84d8
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-04-26 02:14:50 +0500

    login page complete design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import List from "./components/list/List"
-import Chat from "./components/chat/Chat"
-import Detail from "./components/detail/Detail"
+import List from "./components/list/List";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
 import "./App.css";
+import Login from "./components/login/Login";
 
 function App() {
-  return <div className="container">
-    <List />
-    <Chat />
-    <Detail />
-  </div>;
+  const user = false;
+  return (
+    <div className="container">
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import "./login.css";
+import Avatar from "../../assets/avatar.png";
+
+const Login = () => {
+  const [avatar, setAvatar] = useState({
+    file: null,
+    url: "",
+  });
+
+  const handleAvatar = (e) => {
+    if (e.target.files[0]) {
+      setAvatar({
+        ...avatar,
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
+  };
+  return (
+    <div className="login">
+      <div className="item">
+        <h2>Welcome back,</h2>
+        <form>
+          <input type="email" name="email" placeholder="Enter Your  email" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Your password"
+          />
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="separato  h-[80%] w-0.5 border-1 border-[#dddddd35]"></div>
+      <div className="item">
+        <h2>Create An Account,</h2>
+        <form>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter Your Full Name"
+          />
+          <input type="email" name="email" placeholder="Enter Your email" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Your password"
+          />
+          <label htmlFor="file">
+            <img src={avatar.url || Avatar} alt="" />
+            Upload an Image
+          </label>
+          <input
+            type="file"
+            id="file"
+            style={{ display: "none" }}
+            onChange={handleAvatar}
+          />
+
+          <button>Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
